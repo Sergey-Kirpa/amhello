4b0d8a9ce5743205b64fb7b36af9be750cee6163
Sergey Kirpa <Kirpa.Sergey@gmail.com>
2019-11-25 18:54:46 +0200

    Refactoring

--- src/main.cc
+++ src/main.cc
@@ -3,18 +3,18 @@
 #include <string.h>
 
 /**
- * Returns the length of UTF-8 code points including the first byte,
- * or 0 if it is an invalid code point.
+ * \returns the length of a UTF-8 code point that starts at the given byte
+ * \retval 0 indicates an invalid code point
  *
- * \param b0 the first byte of UTF-8 code points.
+ * \param b0 the first byte of a UTF-8 code point
  */
-static inline int
-utf8CodePointsLength(const char b0)
+static inline size_t
+utf8CodePointLength(const char b0)
 {
     if ((b0 & 0x80) == 0)
         return 1;
     if ((b0 & 0xC0) != 0xC0)
-      return 0; // invalid code point
+        return 0; // invalid code point
     if ((b0 & 0xE0) == 0xC0)
         return 2;
     if ((b0 & 0xF0) == 0xE0)
@@ -31,29 +31,42 @@ utf8CodePointsLength(const char b0)
  * definition of UTF-8 goes up to 4-byte code points.
  */
 static bool
-isValidUtf8(const unsigned char* source, int length)
+isValidUtf8CodePoint(const unsigned char* source, const size_t length)
 {
     unsigned char a;
     const unsigned char* srcptr = source + length;
     switch (length) {
-        default: return false;
-        // Everything else falls through when "true"...
-        case 4: if ((a = (*--srcptr)) < 0x80 || a > 0xBF) return false;
-        case 3: if ((a = (*--srcptr)) < 0x80 || a > 0xBF) return false;
-        case 2: if ((a = (*--srcptr)) > 0xBF) return false;
-
+    default:
+        return false;
+    // Everything else falls through when "true"...
+    case 4:
+        if ((a = (*--srcptr)) < 0x80 || a > 0xBF) return false;
+    case 3:
+        if ((a = (*--srcptr)) < 0x80 || a > 0xBF) return false;
+    case 2:
+        if ((a = (*--srcptr)) > 0xBF) return false;
 
         switch (*source) {
-            // no fall-through in this inner switch
-            case 0xE0: if (a < 0xA0) return false; break;
-            case 0xED: if (a > 0x9F) return false; break;
-            case 0xF0: if (a < 0x90) return false; break;
-            case 0xF4: if (a > 0x8F) return false; break;
-            default:   if (a < 0x80) return false; break;
+        // no fall-through in this inner switch
+        case 0xE0:
+            if (a < 0xA0) return false;
+            break;
+        case 0xED:
+            if (a > 0x9F) return false;
+            break;
+        case 0xF0:
+            if (a < 0x90) return false;
+            break;
+        case 0xF4:
+            if (a > 0x8F) return false;
+            break;
+        default:
+            if (a < 0x80) return false;
+            break;
         }
 
-
-        case 1: if (*source >= 0x80 && *source < 0xC2) return false;
+    case 1:
+        if (*source >= 0x80 && *source < 0xC2) return false;
     }
     if (*source > 0xF4)
         return false;
@@ -61,13 +74,13 @@ isValidUtf8(const unsigned char* source, int length)
 }
 
 /**
- * Returns whether the given input is a valid UTF-8 encoding.
+ * \returns whether the given input is a valid (or empty) sequence of UTF-8 code points
  */
 static bool
 isValidUtf8String(const char *source, const char *sourceEnd) {
     while (source < sourceEnd) {
-        const auto length = utf8CodePointsLength(*source);
-        if (length > sourceEnd - source || !isValidUtf8(reinterpret_cast<const unsigned char*>(source), length))
+        const auto length = utf8CodePointLength(*source);
+        if (source + length > sourceEnd || !isValidUtf8CodePoint(reinterpret_cast<const unsigned char*>(source), length))
             return false;
         source += length;
     }
@@ -82,7 +95,7 @@ main (void)
 
   const char *text = "test";
   
-  isValidUtf8String(text, text + strlen(text));
+  bool result = isValidUtf8String(text, text + strlen(text));
 
-  return 0;
+  return result ? 0 : 1;
 }
